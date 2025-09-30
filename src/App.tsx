da66522a1009cef0b6baf50e9ae839a5ad58cd22
水色 <xuezhengfeng@tomo.inc>
2025-10-01 00:18:15 +0800

    feat: just wallet sdk demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import SocialWalletDemo from "./social-wallet-demo";
-import TransactionBuilder from "./transaction-builder";
+// import TransactionBuilder from "./transaction-builder";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      <div className="p-3 bg-[skyblue]">
+      {/* <div className="p-3 bg-[skyblue]">
         demos:
         <a href="/" className="ml-2">
           social-wallet-demo
@@ -14,11 +14,11 @@ function App() {
         <a href="/transaction-builder" className="ml-2">
           transaction-builder
         </a>
-      </div>
+      </div> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<SocialWalletDemo />} />
-          <Route path="/transaction-builder" element={<TransactionBuilder />} />
+          {/* <Route path="/transaction-builder" element={<TransactionBuilder />} /> */}
         </Routes>
       </BrowserRouter>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// Polyfill process for browser environment
+if (typeof globalThis.process === 'undefined') {
+  globalThis.process = require('process');
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
