ccecdb18901184187e43432e48a1c4508b1235fe
水色 <xuezhengfeng@tomo.inc>
2025-10-21 19:39:46 +0800

    feat: buffer bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    nodePolyfills({
+      // Whether to polyfill `node:` protocol imports.
+      protocolImports: true,
+    }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      buffer: "buffer",
     },
   },
   define: {
